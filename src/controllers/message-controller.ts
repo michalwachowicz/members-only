2501f86472def5c1737054b4932ceb5979ad2b09
michalwachowicz <wachowiczmichal1@gmail.com>
2025-09-17 17:16:56 +0200

    feat: create message form

--- src/controllers/message-controller.ts
+++ src/controllers/message-controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import MessageService from "../services/message-service";
 import type { MessageCreate } from "../types/message";
-import type { User } from "../types/user";
+import type { SafeUser, User } from "../types/user";
 
 class MessageController {
   async createMessage(req: Request, res: Response) {
@@ -28,11 +28,26 @@ class MessageController {
       const userId = user.id;
       const messageData: MessageCreate = { userId, title, content };
 
-      const newMessage = await MessageService.createMessage(messageData);
-      res.status(201).json(newMessage);
+      await MessageService.createMessage(messageData);
+      res.redirect("/");
     } catch (error) {
       res.status(500).json({ message: (error as Error).message });
     }
+  }
+
+  async getCreateMessage(req: Request, res: Response) {
+    if (!req.isAuthenticated()) {
+      return res.status(401).json({ message: "Authentication required" });
+    }
+
+    const user = req.user as SafeUser;
+    if (!user.isMember) {
+      return res
+        .status(403)
+        .json({ message: "Membership required to create messages" });
+    }
+
+    res.render("create-message");
   }
 }
 
