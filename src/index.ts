6661c408216adeb892cd466a3ca7a38d2fe5eb46
michalwachowicz <wachowiczmichal1@gmail.com>
2025-09-18 16:22:32 +0200

    feat: style home page

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
